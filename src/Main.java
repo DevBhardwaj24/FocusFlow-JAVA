096f34b2932f8af42413cad0fa2d591e1ed89453
DevBhardwaj24 <devb75028@gmail.com>
2026-01-27 23:31:29 +0530

    Added the Menu feature

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,70 @@
-//Task 1:
+//Task 1: is to define the class i want
+// the feature i want are taskID and task desc.
 
-class Task {
-    String description;
-    boolean isDone;
+//Task 2 is to make a Task manager class to handle all that stuff
 
-    public Task(String description){
-        this.description=description;
+import java.util.*;
+
+
+class Task{
+    int taskID;
+    String taskDesc;
+
+    Task(int taskID,String taskDesc){
+        this.taskID=taskID;
+        this.taskDesc =taskDesc;
+    }
+}
+
+class TaskManager{
+
+
+    // need to make a list of task. that contains id and desc. of task
+    List<Task> tasks=new ArrayList<>();
+
+    // fun to add the task
+    void addTask(int taskID,String taskDesc){
+        tasks.add(new Task(taskID,taskDesc));
     }
 
+    // to display all the task
+    List<Task> displayTask(){
+        return tasks;
 
+    }
 }
+
+
 public class Main {
     public static void main(String[] args) {
-        Task t=new Task("complete this project");
-        System.out.println(t);
+        TaskManager manager=new TaskManager();
+        Scanner sc=new Scanner(System.in);
 
+        while(true){
+            System.out.println("=== To Do App ===");
+            System.out.println("Enter your choice :");
+            System.out.println("1. Add Task");
+            System.out.println("2. View Task");
+            System.out.println("3. Exit");
+            int choice=sc.nextInt();
+
+            if(choice==1){
+                System.out.print("Enter the Task ID: ");
+                int id=sc.nextInt();
+                sc.nextLine();
+                System.out.print("Enter the Task Description: ");
+                String desc=sc.nextLine();
+                manager.addTask(id,desc);
+                System.out.println("Your task added successfully. ");
+            } else if (choice==2) {
+                for(Task t: manager.displayTask()){
+                    System.out.println(t.taskID+" - "+ t.taskDesc);
+                }
+
+            }else{
+                break;
+            }
+        }
 
 
     }
